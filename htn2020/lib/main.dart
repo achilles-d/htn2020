2c0f2357a340d2a4d4e87fbee611482517e90873
Achilles Dabrowski <achilles.dabrowski@gmail.com>
2020-06-06 13:22:35 -0400

    Added user authentication scheme in wrapper using provider

--- htn2020/lib/main.dart
+++ htn2020/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:htn2020/screens/wrapper.dart';
+import 'package:htn2020/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:htn2020/models/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,30 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-
-    /*
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+        child: MaterialApp(
+        home: Wrapper(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      */
-
     );
   }
 }
--- htn2020/lib/screens/wrapper.dart
+++ htn2020/lib/screens/wrapper.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:htn2020/screens/authenticate/authenticate.dart';
 import 'package:htn2020/screens/home/home.dart';
+import 'package:provider/provider.dart';
+import 'package:htn2020/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
+    
     //return either home or authenticate 
     return Authenticate();
   }
