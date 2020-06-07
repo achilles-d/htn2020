c4f3fe466fedd5ba5310237cd198f5b763cb4df4
Achilles Dabrowski <achilles.dabrowski@gmail.com>
2020-06-07 02:36:01 -0400

    Added display of participating businesses from Firebase

--- htn2020/lib/screens/wrapper.dart
+++ htn2020/lib/screens/wrapper.dart
@@ -1,30 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:htn2020/models/gift_card.dart';
 import 'package:htn2020/screens/authenticate/authenticate.dart';
 import 'package:htn2020/screens/home/home.dart';
 import 'package:provider/provider.dart';
 import 'package:htn2020/models/user.dart';
+import 'package:htn2020/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:htn2020/services/database.dart';
 
 class Wrapper extends StatelessWidget {
+  
+  final List<GiftCard> cards = new List<GiftCard>();
+
   @override
   Widget build(BuildContext context) {
-
-    /* 
-    TODO: Query database for collection of dictionaries
-    For each restaurant, create a gift card object with images, names,
-    etc. mapped 1:1
-    */
+    
     final user = Provider.of<User>(context);
-    GiftCard giftCard_1 = new GiftCard("Canape Restaurant", "https://4.bp.blogspot.com/-L9CtV6gR8GI/WtgKA619aEI/AAAAAAAAF9c/CubtyZE94o076qCShJN_D2bdNiHoeIRxACEwYBhgL/s1600/cool%2Bprofile%2Bimages.png",
-     "Campus Drive", "000-000-0000");
-     GiftCard giftCard_2 = new GiftCard("Canape Restaurant", "https://4.bp.blogspot.com/-L9CtV6gR8GI/WtgKA619aEI/AAAAAAAAF9c/CubtyZE94o076qCShJN_D2bdNiHoeIRxACEwYBhgL/s1600/cool%2Bprofile%2Bimages.png",
-     "Campus Drive", "000-000-0000");
-    List<GiftCard> myGiftCardList = [giftCard_1, giftCard_2];
+    
+    getBusinesses();
+    
     //return either home or authenticate 
     if (user == null){
       return Authenticate();
     } else {
-      return Home(myGiftCardList);
+      return Home(cards);
+    }
+  }
+
+  void getBusinesses() async {
+    QuerySnapshot currentBiz  = await Firestore.instance.collection('Businesses').getDocuments();
+    List<DocumentSnapshot> bizDocs = currentBiz.documents;
+    for(var document in bizDocs){
+      cards.add(
+        new GiftCard(
+                    document["name"], 
+                    document["image url"], 
+                    document["address"], 
+                    document["contact"])
+        );
     }
   }
 }
